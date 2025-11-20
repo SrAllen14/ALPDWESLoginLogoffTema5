aef831eab6f8a33db49beac6550cbab8530ea3e9
SrAllen14 <alvaro.allper.1@educa.jcyl.es>
2025-11-20 11:00:01 +0100

    Modificado index principal: creados estilos y estructura principal de la página Creados scripts de la base de datos

---
+++ indexProyectoTema5.php
@@ -0,0 +1,148 @@
+<html lang="es"><head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Indice general de la asignatura">
+    <meta name="author" content="Álvaro Allén Perlines">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <title>Álvaro Allén Perlines</title>
+    <style>
+        
+        *{
+            box-sizing: border-box;
+            margin: 0 auto;
+            padding: 0 auto;
+        }
+        
+        body{
+            width: 1920px;
+            height: 1080px;
+            font-family: Arial, sans-serif;
+            background: #f4f6f9;
+            align-items: center;
+            text-align: center;
+        }
+        
+        nav{
+            background-color: #456D96; 
+            color: white;
+            height: 10%;
+            line-height: 108px;
+        }
+        
+        main{
+            width: 100%;
+            height: 80%;
+        }
+        
+        table.bd{
+            width: 400px;
+            margin-left: auto;
+            margin-right: auto;
+            border-collapse: collapse;
+        }
+        
+        div.tabla2{
+            margin:10px auto 10px auto;
+            padding: 10px;
+            width: 1500px;
+            
+            border-radius: 20px;
+            background-color: white; 
+        }
+        
+        table.ejer{
+            width: 1400px;
+            margin-left: auto;
+            margin-right: auto;
+            border-collapse: collapse;
+            
+        }
+        
+        img{
+            width: 50px;
+            height: 50px;
+        }
+        
+        
+        .principal{
+            font-weight: bold;
+            text-align: center;
+        }
+        
+        td{
+            height: auto;
+            width: auto;
+            border: 1px solid gray;
+        }
+        
+        .imagen{
+            height: 50px;
+            width: 50px;
+        }
+        
+        .ejer td:nth-of-type(1){
+            width: 50px;
+            text-align: center;
+        }
+        
+        footer{
+            margin: auto;
+            background-color: #456d96;
+            text-align: center;
+            align-content: center;
+            height: 10%;
+	    color: white;
+            
+            & a{
+               text-decoration: none; 
+            }
+        }
+                
+        i{
+            width: 10px;
+            height: 10px;
+            color: white;
+        }
+        
+
+    </style>
+</head>
+<body>
+    <nav>
+        <h2>Proyecto Log In - Log Off Tema 5</h2>
+    </nav>
+    <main>
+        <div class="tabla2">
+            <!-- comment 
+            <table class="ejer">
+                <tbody>
+                    <tr class="principal">
+                        <td>Nº</td>
+                        <td>Enunciado</td>
+                        <td>Ejecutar</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td></td>
+                        <td class="imagen"></a></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td></td>
+                        <td class="imagen"></a></td>
+                    </tr>
+                </tbody>
+            </table>-->
+        </div>
+    </main>
+    <footer>
+        <div>
+            <a href="/index.html">
+           Álvaro Allén Perlines
+            </a>
+            <time datetime="2025-11-20">20-11-2025</time>
+            <a href="https://github.com/SrAllen14/ALPDWESLoginLogoffTema5/tree/master" target="blank"><i class="fa-brands fa-github fa-2x"></i></a>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ scriptDB/BorraDBALPDWESProyectoTema5.sql
@@ -0,0 +1,15 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  alvaro.allper.1
+ * Created: 30 oct. 2025
+ */
+
+
+-- En caso de que exista la base de datos se borra --
+DROP DATABASE IF EXISTS DBALPDWESLoginLogoffTema5;
+
+-- En caso de que exista el usuario se borra --
+DROP USER IF EXISTS 'userALPDWESLoginLogoffTema5'@'%';
